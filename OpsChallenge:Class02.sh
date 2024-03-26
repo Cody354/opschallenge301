82329500bf74b1b210670e86bdc4b09f00dff090
Cody Blahnik <153008650+Cody354@users.noreply.github.com>
2024-03-26 16:16:49 -0500

    Update OpsChallenge:Class02.sh

--- OpsChallenge:Class02.sh
+++ OpsChallenge:Class02.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge Append; Date and Time
+# Author:                       Cody Blahnik
+# Date of latest revision:      3/26/24
+# Purpose:                      copys /var/log/syslog and appends time
+
+
+
+#copy sylogs and appends the time and date
 cp /var/log/syslog "syslog_$(date '+%Y-%m-%d_%H-%M-%S')"
