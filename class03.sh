f5f645892d7eec3e5637778f7ab348a17f47fb5d
cody354 <cody.blahnik@gmail.com>
2024-03-27 15:36:19 -0500

    first

--- class03.sh
+++ class03.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Script Name:                 Ops Challenge: File Permissions
-# Author:                       Cody Blahnik
-# Date of latest revision:      3/27/24
-# Purpose:                       create a bash script that alters file permissions of everything in a directory.
-
+# Author:                      Cody Blahnik
+# Date of latest revision:     3/27/24
+# Purpose:                     Create a bash script that alters file permissions of everything in a directory.
 
 # Prompts user for input directory path.
-read -p "what directory path do you want to use?" $dire
+read -p "What directory path do you want to use? " dire
 
 # Prompts user for input permissions number (e.g. 777 to perform a chmod 777)
-read -p "input permissions number (e.g. 777 to perform a chmod 777)?" $per
+read -p "Input permissions number (e.g. 777 to perform a chmod 777): " per
 
 # Navigates to the directory input by the user and changes all files inside it to the input setting.
-cd $dire >> chmod -R $per
+cd "$dire" && chmod -R "$per" .
 
 # Prints to the screen the directory contents and the new permissions settings of everything in the directory.
-
-echo "directory contents and the new permissions settings of everything in the directory."
+echo "Directory contents and the new permissions settings of everything in the directory:"
+ls -l "$dire"
 
