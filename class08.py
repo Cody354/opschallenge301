57fa3bdf63cce08b41be187e2f28d1cc28e87a7d
cody354 <cody.blahnik@gmail.com>
2024-04-03 16:28:42 -0500

    first

---
+++ class08.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Ops Challenge: 08
+# Author:                       Cody Blahnik
+# Date of latest revision:      4/2/24
+# Purpose:                      Python Collections creat a list
+
+
+# Assign to a variable a list of ten string elements.
+the_list = ["apple", "banana", "lettuce", "cake", "berry", "choclate", "grape", "lime", "kiwi", "lemon"]
+
+# Print the fourth element of the list.
+print("Fourth iteam on the list: ", the_list[3])
+
+# Print the sixth through tenth element of the list.
+print("Sixth through tenth elements of the list:", the_list[5])
+
+# Change the value of the seventh element to “onion”.
+the_list[6] = "onion"
+# print the new list
+print("newlist: ", the_list)
